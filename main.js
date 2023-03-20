e6b45040a1cff8d8f7c15174504b34aadb403dae
Toad114514 <xiaolan2332021@163.com>
2023-03-20 12:47:18 +0000

    主文件 无
    Signed-off-by: Toad114514 <xiaolan2332021@163.com>

---
+++ main.js
@@ -0,0 +1,100 @@
+
+//     Copyright (c) 2023 Toad114514
+//     华强买瓜 is licensed under Mulan PSL v2.
+//You can use this software according to the terms and conditions of the Mulan PSL v2. 
+//You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2 
+ //  THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
+ //  See the Mulan PSL v2 for more details.  
+
+toast("该游戏需要控制台才能游玩，如果你看不见控制台，可能是你没有给这个玩意一个悬浮窗权限！")
+console.show();
+console.log("有一个人前来买瓜");
+console.log("哥们，这瓜多少钱一斤啊");
+console.log("你该怎么说呢？ 1两块钱一斤 2十五块钱一斤（填序号）");
+var a =console.input();
+if(a==1){
+    console.log("你这瓜皮子金子做的还是瓜粒子金子做的");
+       var a=0;
+       console.log("1 我这的瓜都是生瓜蛋子 2 你是故意找碴是不是 3 哪有瓜，大棚瓜，你要不要吧");
+          var a=console.input();
+          if(a==1){
+              console.log("what up");
+              console.log("华强劈了你，你没了");
+            console.log("达成成就：实话实说");
+              }
+              if(a==2){
+                  log("因为你态度不好，华强走了");
+                  log("达成成就：服务态度取决于一切");
+                  }else{
+                      console.log("华强看出了一点点问题");
+                      var a=0;
+                      console.log("你要干什么？1 称瓜 2 下手为强 3 召唤援兵");
+                      var a=console.input();
+                      if(a==3){console.log("你召唤了114514位小兵，把华强给吓跑了\n达成成就：以多欺少")}
+                      if(a==1){
+                          console.log("这瓜钥匙不熟你给我吃了它");
+                          console.log("你这称有问题啊(翻过来)");
+                          console.log("1 劈 2 不说话");
+                          var a=0;
+                          var a=console.input();
+                          
+                          if(a==1){console.log("你把华强劈了，被警察抓了。\n达成成就：我再等等")}
+                          if(a==2){
+                              console.log("你说了，这瓜要是不熟你给我吃了它啊(劈瓜)");
+                              console.log("1 你tm劈我瓜是吧 2 吞下去 3 召唤小兵");
+                              var a=console.input();
+                              if(a==1){
+                              console.log("华强拿刀劈了你(萨日郎！x114514)而后灰溜溜的走了\n达成成就：尊重历史");
+                               }
+                               if(a==2){console.log("你吃了下去。晚上，你不得不做了一晚上的食物\n达成成就：说到做到")}
+                               if(a==3){console.log("你召唤了114514位小兵，华强被吓跑了。\n达成成就：以114514敌1")}
+                               if(a==114){console.log("先辈罢工，华强知道你要召唤小兵，把你给劈了\n达成成就：先辈集体罢工罢")}
+                               if(a==54188){console.log("因对华强说54188，被华强劈\n达成成就：我还是你爷爷呢")}
+                          }
+                      }
+                          if(a==2){
+                          console.log("拿什么劈华强？ 1 瓜 2 刀 3 烂贱");
+                          var a=0;
+                          var a=console.input();
+                          if(a==1){
+                              console.log("你用瓜扔向了华强，造成1.14伤害");
+                              console.log("华强拿起刀攻击了你，造成114514伤害，你没了\n达成成就：瓜之屑器");
+                          }
+                              if(a==2){
+                                  console.log("你把华强劈了，接下来....");
+                                  console.log("1 跑 2等待");
+                                  var a=0;
+                                  var a=console.input();
+                                    if(a==1){console.log("你虽然跑了，但还是被抓到了\n达成成就：恶有恶报");};
+                                    if(a==2){console.log("等着等着，发现警察，还是被抓了。\n达成成就：恶有恶报2")};
+                              }
+                                if(a==3){
+                                    console.log("烂贱把你给揍了");
+                                    console.log("获得成就：先下手为强之下手为强");
+                                }
+                                if(a=="114"){
+                                    console.log("你用奇诺比奥(114514)击退了华强！\n达成成就：Toad114514的用处");
+                                    }
+                              }
+                       }
+    }else{
+        console.log("你买的瓜太贵了，华强走了");
+        console.log("达成成就：瓜太贵");
+}
+console.log("结束了\n达成成就：结束了");
+console.log("《华强劈瓜之传》by 奇诺比奥 114514,Power by Auto.js");
+console.log("1 查看全部成就 2 退出");
+var a=console.input();
+if(a==1){
+    console.log("查看所有成就：\n1 实话实说 2 瓜太贵 3 服务态度取决于一切 4 以多欺少 5 我再等等");
+    console.log("6 尊重历史 7 说到做到 8 以114514敌1 9瓜之屑器 10 恶有恶报");
+    console.log("11 恶有恶报2 12 先下手为强之下手为强 13 结束了");
+    console.log("Extra 成就：\n1 先辈集体罢工罢 2 我还是你爷爷呢我 3 Toad114514的用处");
+    console.log("输入2关闭");
+    var c=console.input();
+    if(c==2){ console.hide(); }
+    }
+if(a==2){
+    console.hide();
+    }
